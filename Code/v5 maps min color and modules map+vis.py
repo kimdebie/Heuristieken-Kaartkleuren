eeb0593f8868c321f22deb8c92096676e27d1c01
RikVolger <rvolger@gmail.com>
2015-12-15 14:08:44 +0100

    yay

--- Code/v5 maps min color and modules map+vis.py
+++ Code/v5 maps min color and modules map+vis.py
@@ -12,9 +12,10 @@ import timeit
 import time
 import numpy
 
-# import the visualisation and csv importer
+# import the visualisation, csv importer and benchmark module
 import importvis
 import init_map
+import benchmark
 
 # load the dictionary
 dict_countries = init_map.load_dict(my_map)
@@ -119,8 +120,6 @@ def all_countries_coloured(countries_object):
 
 def algorithm():
 
-    steps = 0
-
     # initiate counter for random seed
     i = 1
 
@@ -136,33 +135,46 @@ def algorithm():
             key = country
 
     # colour first country
-    for colour in countries_object[key].available_colours:
-        country = copy.copy(countries_object[key])
-        country.current_colour = colour
-        country.is_coloured = True
-        stack.append([country])
+    country = copy.copy(countries_object[key])
+    country.current_colour = country.available_colours[len(country.available_colours) - 1]
+    country.is_coloured = True
+    stack.append([country])
 
+    # counter for steps, the first step has just been taken
+    births = 1
+
+    # counter for amount of children created, the first child has just been created.
+    childrencreated = 1
+    
     solution = []
     
     print "starting the algo"
     
     # depth-first
     while (len(stack) != 0):
+
         # new random seed for selecting next country
+        random.seed(time.time() + i)
+        i += 1
+
+        # get parent from stack
         parent = stack.pop()
 
         #print len(parent)
         #print len(countries_object)
         if len(parent) == len(countries_object):
-            #if you want the solution as output
+            # # if you want the solution as output
             #solution = parent
-            #if you want the amount of steps taken as output
-            solution = steps
+            # # if you want the amount of times children were created as output
+            solution = births
+            # # if you want the amount of children created as output
+            # solution = childrencreated
             break
         children = generate_children(parent)
         # if there are no children, there are no solutions for this parent
         if len(children) != 0:
-            steps += len(children)
+            births += 1
+            childrencreated += len(children)
             for child in children:
                 stack.append(child)
 
@@ -189,88 +201,20 @@ def algorithm():
 #solution = algorithm()
 
 #--------------------return the timing of the results--------------------------------------------#
-import timer
-import timeit
 
 steps = []
 
-for i in range(0,10):
-    
-    for j in range(0,10):
-        step = algorithm()
-        print step
-        steps.append(step)
+for i in range(0,100):
+    step = algorithm()
+    print step
+    steps.append(step)
 
     #print max(times), min(times), numpy.mean(times)
     
+print steps
 
-#--------------------------display results in histogram------------------------------------------#
-'''
-Creating a histogram, edited from http://matplotlib.org/1.5.0/examples/animation/histogram.html 
-'''
-"""
-This example shows how to use a path patch to draw a bunch of
-rectangles for an animated histogram
-"""
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.path as path
-import matplotlib.animation as animation
-import numpy
-
-fig, ax = plt.subplots()
-
-# histogram our data with numpy
-data = steps
-n, bins = numpy.histogram(data, 100)
-
-# get the corners of the rectangles for the histogram
-left = numpy.array(bins[:-1])
-right = numpy.array(bins[1:])
-bottom = numpy.zeros(len(left))
-top = bottom + n
-nrects = len(left)
-
-# here comes the tricky part -- we have to set up the vertex and path
-# codes arrays using moveto, lineto and closepoly
-
-# for each rect: 1 for the MOVETO, 3 for the LINETO, 1 for the
-# CLOSEPOLY; the vert for the closepoly is ignored but we still need
-# it to keep the codes aligned with the vertices
-nverts = nrects*(1 + 3 + 1)
-verts = numpy.zeros((nverts, 2))
-codes = numpy.ones(nverts, int) * path.Path.LINETO
-codes[0::5] = path.Path.MOVETO
-codes[4::5] = path.Path.CLOSEPOLY
-verts[0::5, 0] = left
-verts[0::5, 1] = bottom
-verts[1::5, 0] = left
-verts[1::5, 1] = top
-verts[2::5, 0] = right
-verts[2::5, 1] = top
-verts[3::5, 0] = right
-verts[3::5, 1] = bottom
-
-barpath = path.Path(verts, codes)
-patch = patches.PathPatch(
-    barpath, facecolor='red', edgecolor='purple', alpha=0.5)
-ax.add_patch(patch)
-
-ax.set_xlim(left[0], right[-1])
-ax.set_ylim(bottom.min(), top.max())
-
-
-def animate(i):
-    # simulate new data coming in
-    data = steps
-    n, bins = numpy.histogram(data, 100)
-    top = bottom + n
-    verts[1::5, 1] = top
-    verts[2::5, 1] = top
-    return [patch, ]
-
-ani = animation.FuncAnimation(fig, animate, 1, repeat=False, blit=True)
-plt.show()
+# export the amount of steps taken to csv
+benchmark.exportcsv(steps)
 
 # draw the map
-#importvis.Draw_map(solution,my_map,"end")
+#importvis.Visualize(solution,my_map,)
